5705a4e5770d75679f4ad5790b4caf8c00ae4c93
Aron Ruzsa <ruzsa.aron@gmail.com>
2023-05-26 16:37:29 +0200

    Third modification

--- api.js
+++ api.js
@@ -1,29 +1,42 @@
 const superagent = require("superagent");
 const baseUrl = "https://gorest.co.in/public/v2/";
 const TOKEN = require("./resources/token");
-const userData = require("./resources/testdata");
 
-const createUser = async (userData) => {
+const userData = {
+    email: `test${Math.floor(Math.random() * 1000 + 1)}@testemail.com`,
+    name: "Andrew Bernard",
+    gender: "male",
+    status: "inactive"
+};
+
+const newData = {
+    name: "Clark Kent",
+    status: "active"
+};
+
+
+let userId = null;
+
+export const createUser = async (userData) => {
 
     try {
         const res = await superagent
             .post(`${baseUrl}users`)
-            .set("Content-Type", "application/json")
             .set("Authorization", `Bearer ${TOKEN}`)
             .send(userData);
-        return res;
+        userId = res.body.id;
+        return res.body;
     } catch (err) {
         console.log(err);
     }
 }
 
-createUser(userData);
-
-const getUserById = async (userId) => {
+export const getUserById = async (userId) => {
     try {
         const res = await superagent
             .get(`${baseUrl}users/${userId}`)
             .set("Authorization", `Bearer ${TOKEN}`);
+        console.log(res.body);
         return res;
 
     } catch (err) {
@@ -31,9 +44,7 @@ const getUserById = async (userId) => {
     }
 }
 
-//getUserById(1931167)
-
-const updateUser = async(userId, newData) => {
+export const updateUser = async(userId, newData) => {
     try {
         const res = await superagent
             .put(`${baseUrl}users/${userId}`)
@@ -47,15 +58,28 @@ const updateUser = async(userId, newData) => {
 }
 
 
-const deleteUser = async (userId) => {
+export const deleteUser = async (userId) => {
     try {
         const res = await superagent
             .delete(`${baseUrl}users/${userId}`)
+            .set("content-type", "application/json")
             .set("Authorization", `Bearer ${TOKEN}`)
+        console.log(res.body);
         return res;    
     } catch (err) {
         console.log(err);
     }
 }
 
-module.exports = createUser, getUserById, updateUser, deleteUser;
+(async () => {
+    try {
+        await createUser(userData);
+        await getUserById(userId);
+        await updateUser(userId, newData);
+        await deleteUser(userId);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+//module.exports = createUser, getUserById, updateUser, deleteUser;
--- api.test.js
+++ api.test.js
@@ -1,53 +1,48 @@
-const {createUser, getUserById, updateUser, deleteUser} = require('./api');
-const {userData, newData} = require('./resources/testdata') ;
-const {describe, expect, test, it} = require('@jest/globals');
+import {describe, test, it, jest, beforeAll } from '@jest/globals';
+import {createUser, getUserById, updateUser, deleteUser} from "./api.js";
+import { expect } from 'chai';
 
+const userData = {
+    email: `test${Math.floor(Math.random() * 1000 + 1)}@testemail.com`,
+    name: "David Hasselhoff",
+    gender: "male",
+    status: "inactive"
+};
 
-//1931167
+const newData = {
+    name: "Clark Kent",
+    status: "active"
+};
+
 describe("API tests", () => {
-    let userId = null;
+    let userId = 2171623;
     let user = null;
+    
+    test.skip("Create a new user", async () => {
+        const res = await createUser(userData);
+        userId = res.body.id;
+        user = res.body;
+        expect(res.body).to.deep.include(userData);
+        expect(res.status).to.eq(201);
 
-    it("Create a new user", async () => {
-        try {
-            const res = await createUser(userData);
-            userId = res.body.id;
-            user = res.body;
-            expect(res.body).toEqual(expect.objectContaining(userData));
-            expect(res.status).toBe(200);
-        } catch (err) {
-            console.log(err);
-        }
     });
-    
-    it("Get user by ID", async () => {
-        try {
-            const res = await getUserById(userId);
-            expect(res.body).toEqual(user);
-            expect(res.status).toBe(200);
-        } catch (err) {
-            console.log(err);
-        }
+
+    test("Get user by ID", async () => {
+        const res = await getUserById(userId);
+        expect(res.body.gender).to.eq("male");
+        expect(res.status).to.eq(200);
     })
-    
-    it("Update user", async () => {
-        try {
-            const res = await updateUser(userId, newData);
-            expect(res.body).toEqual(expect.objectContaining(newData));
-            expect(res.status).toBe(200);
-        } catch (err) {
-            console.log(err);
-        }
+
+    test("Update user", async () => {
+        const res = await updateUser(userId, newData);
+        expect(res.body).to.deep.include(newData);
+        expect(res.status).to.eq(200);
+
     })
-    
-    it("Delete user", async () => {
-        try {
-            const res = await deleteUser(userId);
-            expect(res.body).toEqual({});
-            expect(res.status).toBe(204);
-        } catch (err) {
-            console.log(err);
-        }
+
+    test("Delete user", async () => {
+        const res = await deleteUser(userId);
+        expect(res.status).to.eq(204);
     })
 })
 
